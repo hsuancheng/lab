2c6e5c5404d1d9722a1887f8153abae0061e8ce5
Hsuan-Cheng Huang <hchuang@HC-Mac-Studio.local>
2026-01-31 13:16:28 +0800

    Fix site URL in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://hchuang.github.io',
+    site: 'https://hsuancheng.github.io',
     base: '/lab',
     outDir: 'dist'
 });
